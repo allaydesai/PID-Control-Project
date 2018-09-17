380cefc4f01bd1ae136177921a8b43e155c84e9b
Allay Desai <allaydesai@gmail.com>
2018-09-17 16:38:43 -0400

    Completed pid.cpp functions and updated main.cpp to initialize and calculate

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,39 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() 
+{
+	p_error = 0.0;
+  	i_error = 0.0;
+	d_error = 0.0;
+	initialized = false;
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double kp, double ki, double kd) 
+{
+	Kp = kp;
+	Ki = ki;
+	Kd = kd;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte) 
+{
+	if (!initialized)
+	{
+		initialized = true;
+		p_error = cte;		
+	}
+
+	d_error = cte - p_error;
+	p_error = cte;
+	i_error += cte;
+
 }
 
-double PID::TotalError() {
+double PID::TotalError() 
+{
+	return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
